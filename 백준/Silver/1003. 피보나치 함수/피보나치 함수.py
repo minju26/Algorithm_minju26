fa3fbd780382e96a308da6aa55bb1935f69eb52f
minju26 <110724254+minju26@users.noreply.github.com>
2024-03-07 11:41:44 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+testCase = [int(input()) for _ in range(n)]
+m = max(testCase)
+
+dp_0 = [0]*(m+1)
+dp_1 = [0]*(m+1)
+
+for i in range(m+1):
+    if i == 0:
+        dp_0[i] = 1
+    elif i == 1:
+        dp_1[i] = 1
+    else:
+        dp_0[i] = dp_0[i-1] + dp_0[i-2]
+        dp_1[i] = dp_1[i-1] + dp_1[i-2]
+
+for i in testCase:
+    print(dp_0[i], dp_1[i])
