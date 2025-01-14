a4b54c56277261d7044a2cbe18d8d7b98dbb4674
minju26 <110724254+minju26@users.noreply.github.com>
2025-01-14 15:45:47 +0900

    [Gold IV] Title: 공유기 설치, Time: 540 ms, Memory: 41164 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,12 +1,10 @@
-# boj 2110 :: 공유기 설치 :: gld.4
-
 import sys
-
 input = sys.stdin.readline
 
-home = []
-
 n, c = map(int, input().split())
+home = []
+ans = -1
+
 for _ in range(n):
     home.append(int(input()))
 
@@ -14,42 +12,23 @@ home.sort()
 
 start = 1
 end = home[-1] - home[0]
-ans = -1
 
 while start <= end:
     mid = (start + end) // 2
-    cur_c = 1
     cur_home = home[0]
-    # nxt_home = -1  => 굳이 이전 집을 계속 기억할 필요가 없다
+    c_cnt = 1
 
-    for i in range(1, n):
-        '''
-        if home[i] - cur_home <= mid:
-            nxt_home = home[i]
-        else:
-            ans = min(ans, cur_home - nxt_home)  => distance를 따로 계산할 필요가 없다. (탐색중인 mid 값으로 답을 결정할 것이므로.)
-            cur_c += 1
-            cur_home = nxt_home
-            nxt_home = home[i]
-        '''
-        
-        # cur_home과 현재 좌표 거리가 mid 보다 같거나 길다면 cur_c 증가
-        # (mid보다 길면 이전 좌표에 공유기 설치/mid보다 같다면 현재 좌표에 공유기 설치 , cur_home을 현재 좌표로 수정)
+    for i in range(n):
+
         if home[i] - cur_home >= mid:
-            cur_c += 1
-            cur_home = home [i]
-        
-    # 모든 집 탐색 후 사용된 공유기의 수가 c 보다 크다면 거리 늘이고(start를 수정)
-    # 작거나 같다면 ans로 기록하고 거리를 줄여봄(end를 수정)
-    if cur_c < c:
+            c_cnt += 1
+            cur_home = home[i]
+    
+    # c_cnt가 c보다 작으면 간격이 너무 길다는 뜻 -> mid를 줄여야 함
+    if c_cnt < c:
         end = mid - 1
     else:
         ans = mid
         start = mid + 1
-        # cur_c == c 의 경우도 여기에 포함되는 이유는, ans에 기록해두기 때문에 기록해두고 업데이트 해보면됨 => 최적을 찾는 것이므로
 
 print(ans)
-
-
-
-        
