d0ba5b64f503b3b75249b2189a177108c3e9beb6
minju26 <110724254+minju26@users.noreply.github.com>
2025-01-15 20:44:18 +0900

    [Silver II] Title: 차이를 최대로, Time: 128 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,20 +1,20 @@
-# boj 10819 :: 차이를 최대로 :: silv.2
-# 가능한 모든 순서의 배열에서 합을 구해본다.
-
 import sys
 from itertools import permutations
 
 input = sys.stdin.readline
 
 n = int(input())
-numbers = list(map(int, input().split()))
+a = list(map(int, input().split()))
 
-max_diff_sum = 0
-for p in permutations(numbers, n):
-    diff_sum = 0
+ans = -1
+
+for p in permutations(a, n):
+    p = list(p)
+    temp = 0
+
     for i in range(n-1):
-        diff_sum += abs(p[i] - p[i+1])
+        temp += abs(p[i] - p[i+1])
     
-    max_diff_sum = max(max_diff_sum, diff_sum)
+    ans = max(ans, temp)
 
-print(max_diff_sum)
+print(ans)
