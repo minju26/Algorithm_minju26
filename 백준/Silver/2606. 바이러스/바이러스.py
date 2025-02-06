a8ef4ae51a6ec41f6f795ed8fe208dfda1b8404e
minju26 <110724254+minju26@users.noreply.github.com>
2025-02-06 15:22:30 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,32 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-m = int(input())
+v = int(input())
+e = int(input())
+adj = [[] for _ in range(v)]
+visited = [False] * v
 
-adj = [[] for _ in range(n)]
-
-for _ in range(m):
+for _ in range(e):
     a, b = map(int, input().split())
-    adj[a-1].append(b-1)
-    adj[b-1].append(a-1)
+    adj[a - 1].append(b - 1)
+    adj[b - 1].append(a - 1)
 
-v = [0] * n
-v[0] = 1
+def dfs(v):
+    visited[v] = True
+    for i in adj[v]:
+        if not visited[i]:
+            dfs(i)
 
-while True:
-    new = False # 새로운 감염이 발생하지 않을 때 까지 진행해야 하므로, flag 역할
+dfs(0)
 
-    for i in range(n):
-        if v[i] == 0:
-            continue
+ans = 0
+for i in range(1, v):
+    if visited[i]:
+        ans += 1
 
-        for j in adj[i]:
-            if v[j] == 0:
-                v[j] = 1
-                new = True
-    
-    if not new:
-        break
-
-print(sum(v) - 1)
+print(ans)
