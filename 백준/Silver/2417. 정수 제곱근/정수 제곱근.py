fec723a1b6f8184789a682602c04d52f2c3ee5fd
minju26 <110724254+minju26@users.noreply.github.com>
2025-02-24 21:08:24 +0900

    [Silver IV] Title: 정수 제곱근, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -6,7 +6,8 @@ input = sys.stdin.readline
 n = int(input())
 
 start = 0
-end = 2 ** 63
+# end = 2 ** 63 => n의 최대 값이 2**63 인데, 제곱근을 구하는 것이므로 가능한 최대 q값은 대략 2 ** 32 정도로 설정해도 된다.
+end = 2 ** 32
 
 ans = 0
 while start <= end:
