11ec391d97ab3ca06b9d972e88cc85fa51a3bb3b
minju26 <110724254+minju26@users.noreply.github.com>
2025-02-26 22:51:34 +0900

    [Silver II] Title: 부분수열의 합, Time: 372 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -9,12 +9,8 @@ sub_ary = []
 
 ans = 0
 for size in range(1, n + 1):
-    sub = list(combinations(ary, size))
-    sub_ary.append(sub)
-
-for sub in sub_ary:
-    for i in range(len(sub)):
-        if sum(sub[i]) == s:
+    for combination in combinations(ary, size):
+        if sum(combination) == s:
             ans += 1
 
 print(ans)
