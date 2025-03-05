559fea9b5ff57509863a1c58b7859b52e7582e43
minju26 <110724254+minju26@users.noreply.github.com>
2025-03-05 16:14:34 +0900

    [Silver III] Title: 염색체, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9342. 염색체/염색체.py
+++ 백준/Silver/9342. 염색체/염색체.py
@@ -7,10 +7,22 @@ rule = ['A', 'B', 'C', 'D', 'E', 'F']
 for _ in range(t):
     str = input().strip()
 
-    if str[0] not in rule or len(str) < 3:
-        print('Good')
+    if len(str) < 3:
+        print("Good")
         continue
 
+    if str[0] != "A":
+        if str[0] not in rule:
+            print("Good")
+            continue
+        str = str[1:]
+    
+    if str[-1] != "C":
+        if str[-1] not in rule:
+            print("Good")
+            continue
+        str = str[:-1]
+    
     g = str[0]
     for i in range(1, len(str)):
         if str[i] == str[i - 1]:
@@ -18,23 +30,7 @@ for _ in range(t):
         else:
             g += str[i]
     
-    if len(g) == 3:
-        if g == "AFC": print("Infected!")
-        else: print("Good")
-    
-    elif len(g) == 4:
-        if g[0 : 3] == "AFC" and g[-1] in rule:
-            print("Infected!")
-        elif g[1 : 4] == "AFC" and g[0] in rule:
-            print("Infected!")
-        else:
-            print("Good")
-    
-    elif len(g) == 5:
-        if g[0] in rule and g[-1] in rule and g[1 : 4] == "AFC":
-            print("Infected!")
-        else:
-            print("Good")
-    
+    if g == "AFC":
+        print("Infected!")
     else:
         print("Good")
