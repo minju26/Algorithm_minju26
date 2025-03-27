7e3ca0c3d151fce08fe19fbb81aec692ba76b9c8
minju26 <110724254+minju26@users.noreply.github.com>
2025-03-27 13:26:01 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 996 ms, Memory: 106832 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+table = [list(map(int, input().split())) for _ in range(n)]
+
+psum = [[0] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        psum[i][j] = table[i - 1][j - 1] + psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1]
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = psum[x2][y2] - psum[x1 - 1][y2] - psum[x2][y1 - 1] + psum[x1 - 1][y1 - 1]
+    
+    print(result)
