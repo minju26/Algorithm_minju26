0b475538a3699de898d88a59cc6078a26f526c00
minju26 <110724254+minju26@users.noreply.github.com>
2025-04-08 09:46:44 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 60 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -3,35 +3,43 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-field = [input().strip() for _ in range(m)]
+field = [""] * m
+for i in range(m):
+    field[i] = input().strip()
 
 def bfs(color):
     visited = [[False] * n for _ in range(m)]
+    dr, dc = [-1, 1, 0, 0], [0, 0, -1, 1]
     q = deque()
     result = 0
 
     for i in range(m):
         for j in range(n):
-            if field[i][j] != color or visited[i][j]:
+            if field[i][j] != color:
                 continue
 
-            q.append((i, j))
-            visited[i][j] = True
-            cnt = 1
+            if not visited[i][j]:
+                q.append((i, j))
+                visited[i][j] = True
+                cnt = 1
 
-            while q:
-                r, c = q.popleft()
+                while len(q) != 0:
+                    r, c = q.popleft()
 
-                for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    nr, nc = r + dr, c + dc
+                    for k in range(4):
+                        nr, nc = r + dr[k], c + dc[k]
 
-                    if 0 <= nr < m and 0 <= nc < n:
-                        if not visited[nr][nc] and field[nr][nc] == color:
-                            visited[nr][nc] = True
+                        if nr < 0 or m <= nr or nc < 0 or n <= nc:
+                            continue
+                        if field[nr][nc] != color:
+                            continue
+
+                        if not visited[nr][nc]:
                             q.append((nr, nc))
+                            visited[nr][nc] = True
                             cnt += 1
 
-            result += cnt * cnt
+                result += cnt * cnt
     return result
 
 print(bfs('W'), bfs('B'))
